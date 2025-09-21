a97d0da90c94e48bb25397c1ff21fa83611aba4a
Zakeer Hussain Mohammad <zakeerhussain074@gmail.com>
2025-09-21 17:45:06 +0530

    Move Angular frontend to weather-ui folder and clean up root

--- docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# Replace API URL in config.json with ENV value
-envsubst < /usr/share/nginx/html/assets/config.template.json > /usr/share/nginx/html/assets/config.json
-
-# Start nginx
-exec nginx -g "daemon off;"
--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient()   // 👈 this is what wires up HttpClient
-  ]
-}).catch(err => console.error(err));
