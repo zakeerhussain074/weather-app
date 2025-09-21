5a2047509ac215f4ff78dcd425b4f108d72b4977
Zakeer Hussain Mohammad <zakeerhussain074@gmail.com>
2025-09-21 17:06:33 +0530

    Add Angular frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { WeatherComponent } from "./weather/weather.component";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [WeatherComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient()   // 👈 this is what wires up HttpClient
+  ]
+}).catch(err => console.error(err));
